34b645b99c5c2b04515a4fe34ea710fdebd52058
Maya Eshed <maya.eshed@post.runi.ac.il>
2024-11-26 00:06:45 +0200

    Algebra styling fixes

--- Algebra.java
+++ Algebra.java
@@ -5,23 +5,23 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+		// Tests some of the operations
+		System.out.println(plus(2,3));   // 2 + 3
+		System.out.println(minus(7,2));  // 7 - 2
+		System.out.println(minus(2,7));  // 2 - 7
+		System.out.println(times(3,4));  // 3 * 4
 		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5 
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6   
-   		System.out.println(sqrt(36));
+		System.out.println(pow(5,3));      // 5^3
+		System.out.println(pow(3,5));      // 3^5
+		System.out.println(div(12,3));   // 12 / 3
+		System.out.println(div(5,5));    // 5 / 5
+		System.out.println(div(25,7));   // 25 / 7
+		System.out.println(mod(25,7));   // 25 % 7
+		System.out.println(mod(120,6));  // 120 % 6
+		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+		System.out.println(sqrt(76123));
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
@@ -82,7 +82,7 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		if (x2 == 0) { 	
-			System.out.print("Impossible ");
+			System.out.print("NaN ");
 			return (-1); // can't divide by 0
 		}
 		int count = 0;
@@ -120,7 +120,7 @@ public class Algebra {
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		if (x2 == 0) { 	
-			System.out.print("Impossible ");
+			System.out.print("NaN ");
 			return (-1); // can't divide by 0
 		}
 		int positiveX2 = (x2 < 0) ? minus(0, x2) : x2;
@@ -139,7 +139,7 @@ public class Algebra {
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		if (x < 0) {
-			System.out.print( "Impossible ");
+			System.out.print( "NaN ");
 			return (-1);			
 		} else if (x == 0 || x == 1) {
 			return x;
